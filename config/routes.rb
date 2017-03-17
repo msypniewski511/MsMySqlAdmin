5be058fb2874d4343a721a14367f23dfdbccf1bd
Maciej Sypniewski <msypniewski511@gmail.com>
2017-03-17 11:21:22 +0100

    Add sessions scaffold and some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  resources :users
   root 'main#index'
+  resources :users
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
   get 'main/index'
   get 'main/abot'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # I tried to divide actions on database server to simillar of REST verbs and resources
   resources :databases, except: [:update, :edit] do
   	collection do
   	  get 'server_info'
@@ -12,8 +20,6 @@ Rails.application.routes.draw do
     resources :tables do
       member do
         get 'show_records'
-        get 'new_column'
-        post 'add_column'
       end
       resources :columns
     end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should prompt for login" do
+    get login_url
+    assert_response :success
+  end
+
+  test "should login" do
+    maciej = users(:one)
+    post login_url, params: { name: maciej.name, password: 'secret' }
+    assert_response :redirect
+    assert_equal maciej.id, session[user_id]
+  end
+
+  test "should not login" do
+    maciej = users(:one)
+    post login_url, params: { name: maciej.name, password: "incorect"}
+    assert_redirected_to login_url
+  end
+
+  test "should logout" do
+    delete logout_url
+    assert_redirected_to login_url
+  end
+
+end
